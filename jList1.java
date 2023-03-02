5407d3197bced891c280c4e0ea47c27ff6d6a665
Raja9790 <126749099+Raja9790@users.noreply.github.com>
2023-03-02 12:01:11 +0530

    Add files via upload

---
+++ jList1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ram;
+
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author Administrator
+ */
+class jList1 {
+
+    static int getSelectedIndex() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void SetModel(DefaultListModel x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    static void setModel(DefaultListModel x) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
